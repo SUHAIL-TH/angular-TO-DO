5cf53f4f4bad648aabe71a03482d1994c1f83182
Suhail TH <suhailth17756@gmail.com>
2023-06-01 21:16:18 +0530

    first commit

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent  {
+  title:string = 'Todo list';
+
+  toggleAddTask(){
+    console.log('toggle')
+  }
+
+}
